5b9e04a1d3a45cd9e47c343b6a54b93506e43587
Matias Dicroce <matias.dicroce@globallogic.com>
2022-01-25 16:12:38 -0300

    Aplicación simple de juego de dados finalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dados';
+  numero1: number =  Math.ceil(Math.random()*6);
+  numero2: number =  Math.ceil(Math.random()*6);
+
+  dadoIzquierda = `../assets/img/dice${this.numero1}.png`
+  dadoDerecha = `../assets/img/dice${this.numero2}.png`
+
+
+  tirarDados():void{
+    this.numero1 = Math.ceil(Math.random()*6);
+    this.numero2 =  Math.ceil(Math.random()*6);
+    this.dadoIzquierda = `../assets/img/dice${this.numero1}.png`
+    this.dadoDerecha = `../assets/img/dice${this.numero2}.png`
+  }
 }
